6803b328bfc4aa6d34207fb7b7c28c5009f56d75
danielqueiroz <danielqueiroz@gmail.com>
2024-01-12 08:53:57 -0300

    feature/add_spring_eureka #14 - Added config property file for rabbit

--- tickets-generator/src/test/java/com/st/ticketsgenerator/TicketsGeneratorApplicationTests.java
+++ tickets-generator/src/test/java/com/st/ticketsgenerator/TicketsGeneratorApplicationTests.java
@@ -2,6 +2,7 @@ package com.st.ticketsgenerator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
